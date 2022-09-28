def10ff5a749ae3057dc7bcc6e1d7c1aeeab401c
Stephan Griesel <sgriesel@gmail.com>
2022-09-28 08:34:19 +0200

    search wip

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -1,12 +1,11 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
     Link
-  } from 'react-router-dom';
+} from 'react-router-dom';
 import Loading from "./Loading";
-
 export default function App() {
-    const [data, setData] = useState(null);
-    const [filteredData, setFilteredData] = useState(null);
+    const [data, setData] = useState([]);
+    const [filtered, setFiltered] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
@@ -14,10 +13,9 @@ export default function App() {
         fetch(`https://api.thedogapi.com/v1/breeds`, {
             method: "GET", // default GET
             headers: {
-                'Content-Type': 'application/json',
-                'x-api-key': `${process.env.REACT_APP_API_KEY}`,
-
-            },
+                "Content-Type": "application/json",
+                "x-api-key": `${process.env.REACT_APP_API_KEY}`
+            }
         })
             .then((response) => {
                 if (!response.ok) {
@@ -29,8 +27,8 @@ export default function App() {
             })
             .then((actualData) => {
                 setData(actualData);
-                setError(null);
-                console.log("data", actualData);
+                setFiltered(actualData);
+                console.log(actualData);
             })
             .catch((err) => {
                 setError(err.message);
@@ -41,26 +39,41 @@ export default function App() {
             });
     }, []);
 
-
-    return <div className="container-fluid">        
-        {loading && <div className="text-center"><Loading /></div>}
-        {error && (
-            <div>{`There is a problem fetching the post data - ${error}`}</div>
-        )}
-        <ul className="row justify-content-center">
-            {data &&
-                data.map(({ id, name, image, bred_for, life_span }) => (
-                    <Link to={"/dog/" + id} className="col-md-4 col-sm-6 card my-3 py-3 border-0">
-                    <li key={id}>
-                        <h3>{name}</h3>
-                        <div className="card-img-top text-center">
-                            <img src={image.url} className="photo w-75" />
-                        </div>
-                        <p>{bred_for}</p>
-                        <p>{life_span}</p>
-                    </li>
+    return (
+        <div className="container-fluid">
+            {loading && <div className="text-center"><Loading /></div>}
+            {error && (
+                <div>{`There is a problem fetching the post data - ${error}`}</div>
+            )}
+            <div>
+                <input
+                    onChange={(event) => {
+                        const value = event.target.value;
+                        console.log(value)
+                        const filtered = data.filter((dog) => dog.name.includes(value));
+                        console.log("filtered", filtered);
+                        setFiltered(filtered);
+                    }}
+                />
+            </div>
+            <ul className="row justify-content-center">
+                {filtered.map(({ id, name, image, bred_for, life_span }) => (
+                    <Link
+                        to={"/dog/" + id}
+                        className="col-md-4 col-sm-6 card my-3 py-3 border-0"
+                        key={name}
+                    >
+                        <li key={id}>
+                            <h3>{name}</h3>
+                            <div className="card-img-top text-center">
+                                <img src={image.url} className="photo w-75" />
+                            </div>
+                            <p>{bred_for}</p>
+                            <p>{life_span}</p>
+                        </li>
                     </Link>
                 ))}
-        </ul>
-    </div>;
+            </ul>
+        </div>
+    );
 }
