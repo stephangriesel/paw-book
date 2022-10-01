898a495047f053c1265a7d25d0b959d62f09214c
Stephan Griesel <sgriesel@gmail.com>
2022-10-01 10:51:08 +0200

    dog favorite condition

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import Sad from '../icons/Sad';
 
 const Favorites = () => {
   const {favorites} = useContext(GlobalContext);
@@ -8,6 +9,7 @@ const Favorites = () => {
     <div className='text-center m-5'>
       <h1>Your Favorites</h1>
     </div>
+    {favorites.length > 0 ? 
     <div>
       {favorites.map(dog => (
         <div key={dog.id}  className='d-flex justify-content-center flex-column align-items-center m-5'>
@@ -15,7 +17,8 @@ const Favorites = () => {
         <p>{dog.name}</p>
         </div>
       ))}
-    </div>
+    </div> : <div className='text-center'>You have not favorited any dogs yet <Sad /></div>
+}
     </>
   )
 }
