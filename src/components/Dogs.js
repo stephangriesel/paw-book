4988567e5b9ce40279a4393c567076b9d8db2bf5
Stephan Griesel <sgriesel@gmail.com>
2022-10-02 13:07:36 +0200

    test

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -74,7 +74,7 @@ export default function App() {
                             const filtered = data.filter((dog) => dog.name.toLowerCase().includes(value));
                             console.log("filtered", filtered);
                             setFiltered(filtered);
-                            window.history.pushState(null, null, '/' );
+                            window.history.pushState(null, null, "?reload=123" );
                         }}
                     />
                     <div className="favorite-box">
