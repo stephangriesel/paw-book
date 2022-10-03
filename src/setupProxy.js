4331ec3d5a74ea3eea031a13f9c87509d3b89940
Stephan Griesel <sgriesel@gmail.com>
2022-10-03 13:07:11 +0200

    test proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
-    'https://api.thedogapi.com/v1/breeds',
+    "https://api.thedogapi.com/v1/breeds",
     createProxyMiddleware({
-      target: 'http://localhost:3000',
+      target: "https://api.thedogapi.com/v1/breeds",
       changeOrigin: true,
     })
   );
